4f7a7566774598ba3d1cd3b2fbcf429e003f896c
Bret Beheim <beheim@gmail.com>
2019-03-28 13:47:40 +0100

    cleaned up the original regression script

--- 7_fit_regressions/fit_regressions.r
+++ 7_fit_regressions/fit_regressions.r
@@ -5,40 +5,77 @@ source("../project_support.r")
 
 dir_init("./temp")
 
-RegrDat <- read.csv("./input/RegrDat.csv", stringsAsFactors = FALSE)
+d <- read.csv("./input/RegrDat.csv", stringsAsFactors = FALSE)
 
-RD1 <- RegrDat[is.na(RegrDat$Lag1) == FALSE,]
-RD2 <- RD1[is.na(RD1$Lag2) == FALSE,]
-# LogistRegrDat = as.data.frame(cbind(RD2$MG,RD2[,c(36,51:54)]))
+drop <- which(is.na(d$Lag1) | is.na(d$Lag2))
+d <- d[-drop, ]
+
 ## replaced this line with an explicit call
-LogistRegrDat <- RD2[,c("NGA", "Time", "MG", "Mean", "Lag1", "Lag2", "Phylogeny", "Space", "MG_missing")]
+d <- d[,c("NGA", "Time", "MG", "Mean", "Lag1", "Lag2", "Phylogeny", "Space", "MG_missing")]
 
-expect_equal(dim(LogistRegrDat), c(801, 6 + 3)) # NGA, Time, MG_missing added
-expect_equal(sum(is.na(LogistRegrDat[,1])), 0)
+expect_equal(dim(d), c(801, 6 + 3)) # NGA, Time, MG_missing added
+expect_equal(sum(is.na(d[,1])), 0)
+expect_equal(length(unique(d$NGA)), 28)
 
-##### Logistic Regression 
-## dat = 1st column response variable (binary), subsequent columns predictors
+m1 <- glm(MG ~ Mean + Lag1 + Lag2 + Phylogeny + Space, data = d, family=binomial(link='logit'))
 
-# publication results here
-
-d <- LogistRegrDat
-
-d$key <- paste(d$NGA, d$Time)
-
-appearances <- d$key[which(d$Lag1 == 0)] # preserve these cases for comparison
-
-reslt <- glm(MG ~ Mean + Lag1 + Lag2 + Phylogeny + Space, data = d, family=binomial(link='logit'))
-
-summary(reslt)
+summary(m1)
 
 # use loose thresholds here
-expect_true(abs(as.numeric(logLik(reslt)) - (-79)) < 1)
-expect_true(abs(coef(reslt)[1] - (-7.9)) < 0.15)
-expect_true(abs(coef(reslt)[2] - (9.9)) < 0.1)
+expect_true(abs(as.numeric(logLik(m1)) - (-79)) < 1)
+expect_true(abs(coef(m1)[1] - (-7.9)) < 0.15)
+expect_true(abs(coef(m1)[2] - (9.9)) < 0.1)
 
-# make a plot to show its the same as what's about to come next
 
-post <- extract.samples(reslt)
+
+
+## second analysis, this time dropping the 0's that were NA's
+
+d <- read.csv("./input/RegrDat.csv", stringsAsFactors = FALSE)
+
+# "NA" the lagged values that were originally missing values
+d$MG[d$MG_missing == 1] <- NA
+
+NGAs <- sort(unique(d$NGA))
+
+for (i in 1:length(NGAs)) {
+  nga_rows <- which(d$NGA == NGAs[i])
+# if the MG_missing is 1, the Lag1 one time step forward must be NA, if exists
+  if (length(nga_rows) > 1) {
+    for (j in 1:(length(nga_rows) - 1)) {
+      if (d$MG_missing[nga_rows[j]] == 1) d$Lag1[nga_rows[j + 1]] <- NA
+    }
+  }
+# if the MG_missing is 1, the Lag2 two time steps forward must be NA, if exists
+  if (length(nga_rows) > 2) {
+    for (j in 1:(length(nga_rows) - 2)) {
+      if (d$MG_missing[nga_rows[j]] == 1) d$Lag2[nga_rows[j + 2]] <- NA
+    }
+  }
+}
+
+# d[which(d$NGA == NGAs[i]),c("NGA", "Time", "MG", "Lag1", "Lag2", "MG_missing")]; i <- i + 1
+
+# so corrected, the lag variables have to be removed because they are almost 
+# a perfect clone of the outcome variable
+
+table(d$Lag1, d$MG)
+
+dm <- d[,c("NGA", "Time", "MG", "Mean", "Phylogeny", "Space")]
+
+dm <- dm[-which(is.na(dm$MG)), ]
+
+m2 <- glm(MG ~ Mean + Phylogeny + Space, data = dm, family=binomial(link='logit'))
+
+summary(m2)
+
+# results look numerically unstable; add varying effects?
+
+
+
+
+
+post <- extract.samples(m1)
 
 scs <- seq(0, 1, by = 0.01)
 
@@ -63,6 +100,12 @@ has_mg <- logistic(post$Intercept + post$Lag1 * 0 + post$Mean * 0.42)
 mean(has_mg)
 sd(has_mg)
 
+
+d <- read.csv("./input/RegrDat.csv", stringsAsFactors = FALSE)
+
+drop <- which(is.na(d$Lag1) | is.na(d$Lag2))
+d <- d[-drop, ]
+
 # calculation posterior distributions on p for each case
 d$pr_mg_mean <- NA
 d$pr_mg_sd <- NA
@@ -84,9 +127,7 @@ for (i in 1:nrow(d)) {
   d$pr_mg_ub[i] <- HPDI(has_mg)[2]
 }
 
-png("./temp/pred_comparison.png", res = 300, height = 5, width = 10, units = "in")
-
-par(mfrow = c(1, 2))
+png("./temp/m1_glm_missingness.png", res = 300, height = 5, width = 5, units = "in")
 
 plot(scs, has_mg_mean, ylim = c(0, 1), type = "l",
   ylab = "pr(moralizing gods)", xlab = "social complexity",
@@ -95,7 +136,7 @@ plot(scs, has_mg_mean, ylim = c(0, 1), type = "l",
 polygon(c(scs, rev(scs)), c(has_mg_ub, rev(has_mg_lb)),
   border = NA, col = col.alpha("dodgerblue", 0.2))
 
-tar <- which(d$key %in% appearances)
+tar <- which(d$Lag1 == 0)
 print(paste(length(tar), "cases after lag1 = 0"))
 points(d$Mean[tar], d$pr_mg_mean[tar], pch = 16,
   col = col_alpha("black", 0.5))
@@ -113,7 +154,7 @@ ymax <- 0.95
 
 # prep variables on the new scale
 
-d$logPop <- RD2$PolPop
+d$logPop <- d$PolPop
 d$Pop <- 10^d$logPop
 
 d$Mean2 <- (d$Mean - 0) / 1 * (xmax - xmin) + xmin
@@ -125,9 +166,9 @@ d$na_col <- ifelse(d$MG_missing == 1, "gray", "black")
 
 mg <- 0.02 # don't mix up with outcome variable `MG`!
 
-tar <- which(d$key %in% appearances & d$MG_missing == 1)
+tar <- which(d$Lag1 == 0 & d$MG_missing == 1)
 points(d$Mean2[tar], d$logPop2[tar], pch = 16, col = col_alpha(d$na_col[tar], 0.8))
-tar <- which(d$key %in% appearances & d$MG_missing == 0)
+tar <- which(d$Lag1 == 0 & d$MG_missing == 0)
 points(d$Mean2[tar], d$logPop2[tar], pch = 16, col = col_alpha(d$na_col[tar], 0.8))
 
 lines(c(xmin, xmin) - mg, c(ymin, ymax))
@@ -160,58 +201,14 @@ m0 <- lm(logPop2 ~ Mean2, data = d)
 curve(coef(m0)[1] + coef(m0)[2] * x, from = xmin,
   to = xmax, lty = 2, add = TRUE)
 
+dev.off()
 
 
 
 
-## second analysis, this time dropping the 0's that were NA's
-
-RegrDat <- read.csv("./input/RegrDat.csv", stringsAsFactors = FALSE)
-
-# put the NA's back
-RegrDat$MG[RegrDat$MG_missing == 1] <- NA
-
-# kill lagged variables that were missing values
-
-NGAs <- sort(unique(RegrDat$NGA))
-
-for (i in 1:length(NGAs)) {
-  nga_rows <- which(RegrDat$NGA == NGAs[i])
-# if the MG_missing is 1, the Lag1 one time step forward must be NA, if exists
-  if (length(nga_rows) > 1) {
-    for (j in 1:(length(nga_rows) - 1)) {
-      if (RegrDat$MG_missing[nga_rows[j]] == 1) RegrDat$Lag1[nga_rows[j + 1]] <- NA
-    }
-  }
-# if the MG_missing is 1, the Lag2 two time steps forward must be NA, if exists
-  if (length(nga_rows) > 2) {
-    for (j in 1:(length(nga_rows) - 2)) {
-      if (RegrDat$MG_missing[nga_rows[j]] == 1) RegrDat$Lag2[nga_rows[j + 2]] <- NA
-    }
-  }
-}
-
-# RegrDat[which(RegrDat$NGA == NGAs[i]),c("NGA", "Time", "MG", "Lag1", "Lag2", "MG_missing")]; i <- i + 1
-
-## replaced this line with an explicit call
-LogistRegrDat <- RegrDat[,c("NGA", "Time", "MG", "Mean", "Lag1", "Lag2", "Phylogeny", "Space", "MG_missing")]
-
-d <- LogistRegrDat
-
-d$key <- paste(d$NGA, d$Time)
 
 
-mg_na <- which(is.na(d$MG))
-dm <- d[-mg_na, ]
-
-reslt <- glm(MG ~ Mean + Space, data = d, family=binomial(link='logit'))
-
-summary(reslt)
-
-
-# make a plot to show its the same as what's about to come next
-
-post <- extract.samples(reslt)
+post <- extract.samples(m2)
 
 scs <- seq(0, 1, by = 0.01)
 
@@ -228,6 +225,12 @@ for (i in 1:length(scs)) {
   has_mg_ub[i] <- HPDI(has_mg)[2]
 }
 
+
+d <- read.csv("./input/RegrDat.csv", stringsAsFactors = FALSE)
+
+drop <- which(d$MG_missing == 0)
+d <- d[-drop, ]
+
 # calculation posterior distributions on p for each case
 d$pr_mg_mean <- NA
 d$pr_mg_sd <- NA
@@ -238,6 +241,7 @@ for (i in 1:nrow(d)) {
   has_mg <- logistic(
     post$Intercept +
     post$Mean * d$Mean[i] +
+    post$Phylogeny * d$Phylogeny[i] +
     post$Space * d$Space[i]
   )
   d$pr_mg_mean[i] <- mean(has_mg)
@@ -246,21 +250,23 @@ for (i in 1:nrow(d)) {
   d$pr_mg_ub[i] <- HPDI(has_mg)[2]
 }
 
+
+
+
+png("./temp/m2_glm_missingness.png", res = 300, height = 5, width = 5, units = "in")
+
 plot(scs, has_mg_mean, ylim = c(0, 1), type = "l",
   ylab = "pr(moralizing gods)", xlab = "social complexity",
-  main = "analysis without imputed outcomes")
+  main = "m2 predictions on missing values")
 
 polygon(c(scs, rev(scs)), c(has_mg_ub, rev(has_mg_lb)),
   border = NA, col = col.alpha("dodgerblue", 0.2))
 
-all(appearances %in% d$key) # use `appearances` above to id cases we want to predict
-tar <- which(d$key %in% appearances)
-print(paste(length(tar), "cases with same IDs in new version"))
-points(d$Mean[tar], d$pr_mg_mean[tar], pch = 16,
+points(d$Mean, d$pr_mg_mean, pch = 16,
   col = col_alpha("black", 0.5))
 
-# for (i in 1:length(tar)) lines(c(d$Mean[tar[i]], d$Mean[tar[i]]),
-#   c(d$pr_mg_lb[tar[i]], d$pr_mg_ub[tar[i]]))
+for (i in 1:nrow(d)) lines(c(d$Mean[i], d$Mean[i]),
+  c(d$pr_mg_lb[i], d$pr_mg_ub[i]))
 
 dev.off()
 
